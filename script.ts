608fc7f3617bb4f0a0b78e99be58fd50ca8c92bb
TamKanitnon <TamKanitnon@hotmail.com>
2023-06-11 05:55:11 +0700

    web-example4 V1.1

--- script.ts
+++ script.ts
@@ -1,7 +1,7 @@
 const menutab: any = document.querySelector(".menu-tab");
 const menuhide: any = document.querySelector(".menu-hide");
 
-menutab?.addEventListener("click", () => {
+menutab.addEventListener("click", () => {
     menuhide.classList.toggle('show');
     menutab.classList.toggle('active');
 })
